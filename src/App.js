de019a6b438ed6d489db2e1c2bfce3359f339962
Ryan Short <shorttryan@gmail.com>
2022-09-27 20:17:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function ImageForm(){
+    return(
+      <form>
+        ...
+      </form>
+    )
+};
+
+function ImageList(){
+    return(
+      <ul>
+        ...
+      </ul>
+    )
+};
+
+function ImageBoard() {
+    return(
+      <div>
+        <ImageForm />
+        <ImageList />
+      </div>
+    )
+};
 }
 
 export default App;
